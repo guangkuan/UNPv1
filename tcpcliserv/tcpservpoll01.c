9e11a1dc911f898187685757a490033521e043ac
guangkuan <guangkuo_527@126.com>
2019-03-10 20:43:32 +0800

    Chapter 6 I/O Multiplexing

--- tcpcliserv/tcpservpoll01.c
+++ tcpcliserv/tcpservpoll01.c
@@ -1,19 +1,22 @@
 /* include fig01 */
 #include	"unp.h"
-#include	<limits.h>		/* for _SC_OPEN_MAX */
+#include	<limits.h>		/* for isom */
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int					i, maxi, listenfd, connfd, sockfd;
 	int					nready;
 	ssize_t				n;
 	char				buf[MAXLINE];
 	socklen_t			clilen;
-	struct pollfd		client[_SC_OPEN_MAX];
+	/*/在select的版本，我们必须分配一个client数组以及一个名为rset的描述符集。
+	/改用poll后，我们只需分配一个pollfd结构的数组来维护客户信息，而不必分配另外一个数组。
+	*/
+	int isom = sysconf(_SC_OPEN_MAX);
+	struct pollfd		client[isom];
 	struct sockaddr_in	cliaddr, servaddr;
 
-	//_SC_OPEN_MAX为每个进程可以打开的最大文件数
+	//isom为每个进程可以打开的最大文件数
 	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
@@ -24,34 +27,47 @@ main(int argc, char **argv)
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
 	Listen(listenfd, LISTENQ);
-
+	//把client数组的第一项用于监听套接字，并把其余各项的描述符成员置为-1
 	client[0].fd = listenfd;
-	client[0].events = POLLRDNORM;
-	for (i = 1; i < _SC_OPEN_MAX; i++)
+	//给第一项设置POLLIN事件，这样当由新的连接准备好被接受时poll将通知我们
+	client[0].events = POLLIN;
+	for (i = 1; i < isom; i++)
 		client[i].fd = -1;		/* -1 indicates available entry */
+	//maxi为client数组当前正在使用的最大下标值
 	maxi = 0;					/* max index into client[] array */
 /* end fig01 */
 
 /* include fig02 */
-	for ( ; ; ) {
+	for ( ; ; ) 
+	{
+		/*/poll提供的功能与select类似，不过在处理流设备时，它能够提供额外的信息
+		/第一个参数是指向一个结构数组第一个元素的指针。每个数组元素都是一个pollfd结构，用于指定测试某个给定描述符fd的条件;
+		要测试的条件由events成员指定，函数在相应的revents成员中返回该描述符的状态。（每个描述符都有两个变量，一个为调用值，另一个为返回结果，从而避免使用值-结果参数）
+		/结构数组中元素的个数时由nfds（第二个）参数指定
+		/timeout（第三个）参数指定poll函数返回前等待多久时间*/
 		nready = Poll(client, maxi+1, INFTIM);
 
-		if (client[0].revents & POLLRDNORM) {	/* new client connection */
+		//当一个新的连接被接受后
+		if (client[0].revents & POLLIN) 
+		{	/* new client connection */
 			clilen = sizeof(cliaddr);
 			connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
 #ifdef	NOTDEF
 			printf("new client: %s\n", Sock_ntop((SA *) &cliaddr, clilen));
 #endif
-
-			for (i = 1; i < _SC_OPEN_MAX; i++)
-				if (client[i].fd < 0) {
+			/*在client数组中查找第一个描述符成员为负的可用项。
+			从下标1开始搜索，因为client[0]固定用于监听套接字。
+			*/
+			for (i = 1; i < isom; i++)
+				if (client[i].fd < 0) 
+				{
 					client[i].fd = connfd;	/* save descriptor */
 					break;
 				}
-			if (i == _SC_OPEN_MAX)
+			if (i == isom)
 				err_quit("too many clients");
 
-			client[i].events = POLLRDNORM;
+			client[i].events = POLLIN;
 			if (i > maxi)
 				maxi = i;				/* max index in client[] array */
 
@@ -59,28 +75,37 @@ main(int argc, char **argv)
 				continue;				/* no more readable descriptors */
 		}
 
-		for (i = 1; i <= maxi; i++) {	/* check all clients for data */
+		//检查已连接套接字可读信息
+		for (i = 1; i <= maxi; i++) 
+		{	/* check all clients for data */
 			if ( (sockfd = client[i].fd) < 0)
 				continue;
-			if (client[i].revents & (POLLRDNORM | POLLERR)) {
-				if ( (n = read(sockfd, buf, MAXLINE)) < 0) {
-					if (errno == ECONNRESET) {
+			if (client[i].revents & (POLLIN | POLLERR)) 
+			{
+				if ( (n = read(sockfd, buf, MAXLINE)) < 0) 
+				{
+					if (errno == ECONNRESET) 
+					{
 							/*4connection reset by client */
 #ifdef	NOTDEF
 						printf("client[%d] aborted connection\n", i);
 #endif
 						Close(sockfd);
 						client[i].fd = -1;
-					} else
+					} 
+					else
 						err_sys("read error");
-				} else if (n == 0) {
+				} 
+				else if (n == 0) 
+				{
 						/*4connection closed by client */
 #ifdef	NOTDEF
 					printf("client[%d] closed connection\n", i);
 #endif
 					Close(sockfd);
 					client[i].fd = -1;
-				} else
+				} 
+				else
 					Writen(sockfd, buf, n);
 
 				if (--nready <= 0)
--- tcpcliserv/tcpservselect01.c
+++ tcpcliserv/tcpservselect01.c
@@ -1,8 +1,7 @@
 /* include fig01 */
 #include	"unp.h"
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int					i, maxi, maxfd, listenfd, connfd, sockfd;
 	int					nready, client[FD_SETSIZE];
@@ -25,6 +24,7 @@ main(int argc, char **argv)
 
 	maxfd = listenfd;			/* initialize */
 	maxi = -1;					/* index into client[] array */
+	//整型数组client含有每个客户的已连接套接字描述符，所有元素被初始化为-1
 	for (i = 0; i < FD_SETSIZE; i++)
 		client[i] = -1;			/* -1 indicates available entry */
 	FD_ZERO(&allset);
@@ -32,12 +32,19 @@ main(int argc, char **argv)
 /* end fig01 */
 
 /* include fig02 */
-	for ( ; ; ) {
+	for ( ; ; ) 
+	{
+		//拷贝不修改原结构体
 		rset = allset;		/* structure assignment */
+		//select等待某个事件发生：或是新客户连接的建立，或是数据、FIN或RST的到达
 		nready = Select(maxfd+1, &rset, NULL, NULL, NULL);
 
-		if (FD_ISSET(listenfd, &rset)) {	/* new client connection */
+		if (FD_ISSET(listenfd, &rset)) 
+		{	/* new client connection */
 			clilen = sizeof(cliaddr);
+			/*如果监听套接字变为可读（就绪），那么已建立了一个新的连接。
+			我们调用accept并相应地更新数据结构
+			*/
 			connfd = Accept(listenfd, (SA *) &cliaddr, &clilen);
 #ifdef	NOTDEF
 			printf("new client: %s, port %d\n",
@@ -45,8 +52,10 @@ main(int argc, char **argv)
 					ntohs(cliaddr.sin_port));
 #endif
 
+			//使用client数组中的第一个未用项记录这个已连接描述符
 			for (i = 0; i < FD_SETSIZE; i++)
-				if (client[i] < 0) {
+				if (client[i] < 0) 
+				{
 					client[i] = connfd;	/* save descriptor */
 					break;
 				}
@@ -59,20 +68,31 @@ main(int argc, char **argv)
 			if (i > maxi)
 				maxi = i;				/* max index in client[] array */
 
+			//就绪描述符数目减1，若其值变为0，就可以避免进入下一个for循环，这样做让我们可以使用select的返回值来避免检查未就绪的描述符
 			if (--nready <= 0)
 				continue;				/* no more readable descriptors */
 		}
 
-		for (i = 0; i <= maxi; i++) {	/* check all clients for data */
+		for (i = 0; i <= maxi; i++) 
+		{	/* check all clients for data */
+			//对于每个现有的客户连接
 			if ( (sockfd = client[i]) < 0)
 				continue;
-			if (FD_ISSET(sockfd, &rset)) {
-				if ( (n = Read(sockfd, buf, MAXLINE)) == 0) {
+			//我们要测试其描述符是否在select返回的描述符集中
+			if (FD_ISSET(sockfd, &rset)) 
+			{
+				/*/如果是就从该客户读入一行文本并回射给它
+				/存在问题：如果有恶意的客户连接到服务器，发送一个字节的数据（不是换行符）后进入睡眠（Dos攻击）
+				*/
+				if ( (n = Read(sockfd, buf, MAXLINE)) == 0) 
+				{
 						/*4connection closed by client */
+					//如果该客户关闭了连接，那么就更新相应的数据结构
 					Close(sockfd);
 					FD_CLR(sockfd, &allset);
 					client[i] = -1;
-				} else
+				} 
+				else
 					Writen(sockfd, buf, n);
 
 				if (--nready <= 0)
